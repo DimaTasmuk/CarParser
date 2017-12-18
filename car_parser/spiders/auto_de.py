ea6a56cc7b8393be7d8c9166096aed09518088db
DimaTasmuk <dimatasmuk@gmail.com>
2017-12-18 17:03:32 +0200

    Fix problem with title. Parse price and info about dealer. Move to the next page

--- car_parser/spiders/auto_de.py
+++ car_parser/spiders/auto_de.py
@@ -7,6 +7,8 @@ class AutoDeParser(scrapy.Spider):
                   "vt%5B%5D=1&vt%5B%5D=2&vt%5B%5D=3&vt%5B%5D=4&vt%5B%5D=5&vt%5B%5D=6&vt%5B%5D=7&"
                   "vt%5B%5D=8&vt%5B%5D=99&searchFast=Fahrzeug+suchen&srtcbd=0_asc"]
 
+    list_cars_info = list()
+
     def parse(self, response):
         yield response.follow("#brandModelLayer", self.parse_brands)
 
@@ -18,37 +20,60 @@ class AutoDeParser(scrapy.Spider):
                 yield response.follow(url, self.parse_cars)
 
     def parse_cars(self, response):
-        cars = response.css("ul.vehicleList li.offers")
+        cars = response.css("ul.vehicleList li.contentDesc a.vehicleOffersBox")
         for car in cars:
-            #TODO: problem with title. Sometimes not found headline
-            title = car.css(".headline::text").extract_first().strip()
-            vehicle_data = car.css("technicalData p")
             try:
-                registrationDate = vehicle_data.xpath("//span[@data-content=registrationDate]").extract_first()
+                title = car.css(".headline::text").extract_first().strip()
+            except:
+                print "adgadg"
+            vehicle_data = car.css("div.technicalData p span")
+            try:
+                registrationDate = vehicle_data.css("span[data-content*=registrationDate]::text").extract_first().strip()
             except AttributeError:
                 registrationDate = ''
             try:
-                mileage = vehicle_data.xpath("//span[@data-content=mileage]").extract_first()
+                mileage = vehicle_data.css("span[data-content*=mileage]::text").extract_first().strip()
             except AttributeError:
                 mileage = ''
             try:
-                power = vehicle_data.xpath("//span[@data-content=power]").extract_first()
+                power = vehicle_data.css("span[data-content*=power]::text").extract_first().strip()
             except AttributeError:
                 power = ''
             try:
-                fuelType = vehicle_data.xpath("//span[@data-content=fuelType]").extract_first()
+                fuelType = vehicle_data.css("span[data-content*=fuelType]::text").extract_first().strip()
             except AttributeError:
                 fuelType = ''
             try:
-                gearbox = vehicle_data.xpath("//span[@data-content=gearbox]").extract_first()
+                gearbox = vehicle_data.css("span[data-content*=gearbox]::text").extract_first().strip()
             except AttributeError:
                 gearbox = ''
+            price = response.css("span.priceBig::text").extract_first().strip()
 
-            yield {
-                'title': title,
-                "registrationDate": registrationDate,
-                "mileage": mileage,
-                "power": power,
-                "fuelType": fuelType,
-                "gearbox": gearbox
-            }
+            vehicleOffersDealer = car.css("div.vehicleOffersDealer div p")
+            seller = vehicleOffersDealer[1].css("p::text").extract_first().strip()
+            seller_city = vehicleOffersDealer[1].css("span[data-content*=city]::text").extract_first().strip()
+            seller_phone = vehicleOffersDealer[1].css("p::text").extract()[-1].strip()
+
+            info = 'title:{}, registrationDate:{}, mileage:{}, power:{}, fuelType:{}, gearbox:{}, price:{},' \
+                   'seller:{}, seller_city:{}, seller_phone:{}'. \
+                format(title.encode('utf-8'), registrationDate.encode('utf-8'), mileage.encode('utf-8'),
+                       power.encode('utf-8'), fuelType.encode('utf-8'), gearbox.encode('utf-8'), price.encode('utf-8'),
+                       seller.encode('utf-8'), seller_city.encode('utf-8'), seller_phone.encode('utf-8'))
+            if info not in self.list_cars_info:
+                self.list_cars_info.append(info)
+                yield {
+                    'title': title,
+                    "registrationDate": registrationDate,
+                    "mileage": mileage,
+                    "power": power,
+                    "fuelType": fuelType,
+                    "gearbox": gearbox,
+                    "price": price,
+                    "seller": seller,
+                    "seller_city": seller_city,
+                    "seller_phone": seller_phone
+                }
+
+        next_page = response.css("div.pagNext a.icon-right-dir::attr(href)").extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse_cars)
