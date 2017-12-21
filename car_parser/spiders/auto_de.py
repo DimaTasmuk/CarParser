1fdd2623e76318d027303a8eb89487ee2c0a8eff
DimaTasmuk <dimatasmuk@gmail.com>
2017-12-21 13:14:27 +0200

    Test

--- car_parser/spiders/auto_de.py
+++ car_parser/spiders/auto_de.py
@@ -8,21 +8,26 @@ class AutoDeParser(scrapy.Spider):
     start_urls = ["http://www.auto.de/search/findoffer?sci%5B%5D=&spra=&sma=&sg=&srdi=&sft=&sz=&src=1&"
                   "vt%5B%5D=1&vt%5B%5D=2&vt%5B%5D=3&vt%5B%5D=4&vt%5B%5D=5&vt%5B%5D=6&vt%5B%5D=7&"
                   "vt%5B%5D=8&vt%5B%5D=99&searchFast=Fahrzeug+suchen&srtcbd=0_asc"]
+    brand_id = ['11149', '51', '11220']
 
     list_cars_info = list()
+
+    def __init__(self, brand_id, **kwargs):
+        self.brand_id = brand_id
+        super(AutoDeParser, self).__init__(brand_id, **kwargs)
 
     def parse(self, response):
         yield response.follow("#brandModelLayer", self.parse_brands)
 
     def parse_brands(self, response):
-        try:
-            brand_id = [response.url.split("brand_id=")[1].split("&")[0]]
-        except:
-            brand_id = ['11149', '51', '11220']
-        print(brand_id)
+        # try:
+        #     brand_id = [response.url.split("brand_id=")[1].split("&")[0]]
+        # except:
+        #     brand_id = ['11149', '51', '11220']
+        print(self.brand_id)
         model_keys = set(response.css("div.brandModelLayer div.autoForm ul.brandModel").xpath('//select[@id="sci"]').css("select.brandSearch option::attr(value)").extract())
         for model_key in model_keys:
-            if model_key in [brand_id]:
+            if model_key in [self.brand_id]:
                 url = response.url.split("sci%5B%5D=")[0] + "sci%5B%5D=" + model_key + response.url.split("sci%5B%5D=")[1]
                 yield response.follow(url, self.parse_cars)
 
