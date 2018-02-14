f631c953cd8fecd88dc1d8617f3d778a51d31ade
DimaTasmuk <dimatasmuk@gmail.com>
2018-02-14 15:50:22 +0200

    Change parsed data on SearchPage

--- car_parser/items/AutoItem.py
+++ car_parser/items/AutoItem.py
@@ -3,30 +3,14 @@ import scrapy
 
 class AutoItem(scrapy.Item):
     id = scrapy.Field()
-    title = scrapy.Field()
-    price_brutto = scrapy.Field()
-    price_netto = scrapy.Field()
-
-    details_link = scrapy.Field()
-    image_url = scrapy.Field()
-
-    reg_date = scrapy.Field()
+    marketing_headline = scrapy.Field()
+    sales_price_incl_vat = scrapy.Field()
+    sales_price_excl_vat = scrapy.Field()
     mileage = scrapy.Field()
-    fuel_type = scrapy.Field()
-    fuel_consumption = scrapy.Field()
+    power_in_kw = scrapy.Field()
+    power_in_ps = scrapy.Field()
+    fuel = scrapy.Field()
+    fuel_consumption_comb = scrapy.Field()
     co2_emission = scrapy.Field()
-    power_kW = scrapy.Field()
-    power_PS = scrapy.Field()
-    gearbox_type = scrapy.Field()
-    state = scrapy.Field()
-
-    seller_company = scrapy.Field()
-    seller_location = scrapy.Field()
-    seller_phone = scrapy.Field()
-
-    # don't have
-    # engine_capacity = scrapy.Field()
-    # electricity_consumption = scrapy.Field()
-    # number_of_previous_owners = scrapy.Field()
-    # seller_country = scrapy.Field()
-    # seller = scrapy.Field()
+    gearbox = scrapy.Field()
+    first_registration = scrapy.Field()
--- car_parser/loaders/AutoLoader.py
+++ car_parser/loaders/AutoLoader.py
@@ -2,11 +2,14 @@ from scrapy.loader import ItemLoader
 from scrapy.loader.processors import TakeFirst, MapCompose, Compose, Join
 
 
+def replace_dot(number):
+    return number[0].replace(".", "")
+
+
 class AutoLoader(ItemLoader):
     default_input_processor = MapCompose(unicode.strip)
     default_output_processor = TakeFirst()
 
-    price_brutto_out = Compose(lambda p: int(p[0].replace('.', '')))
-    price_netto_out = Compose(lambda p: int(p[0].replace('.', '')))
-    details_link_out = Join(separator='')
-    seller_phone_out = Compose(lambda v: v[-1], unicode.strip)
+    sales_price_incl_vat_out = Compose(replace_dot)
+    sales_price_excl_vat_out = Compose(replace_dot)
+    mileage_out = Compose(replace_dot)
