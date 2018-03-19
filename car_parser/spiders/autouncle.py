3b4843017cc3c1a4c1d70d45721f47154f86ed0f
Dmytro <dimatasmuk@gmail.com>
2018-03-19 21:33:55 +0200

    Remove logger

--- car_parser/spiders/autouncle.py
+++ car_parser/spiders/autouncle.py
@@ -32,7 +32,6 @@ class AutoUncleParser(scrapy.Spider):
         super(AutoUncleParser, self).__init__(**kwargs)
         if hasattr(self, 'deep'):
             if self.deep.lower() == 'true':
-                self.logger.info("deep parse enabled")
                 self.deep_parse_enabled = True
 
     def parse(self, response):
@@ -153,7 +152,6 @@ class AutoUncleParser(scrapy.Spider):
                                                        PARAMETERS['model'])
                 )
                 if self.deep_parse_enabled:
-                    self.logger.info("in parse car")
                     request = scrapy.Request(response.urljoin(origin_link),
                                              self.parse_car_details)
                     request.meta['meta_model'] = model
@@ -184,7 +182,6 @@ class AutoUncleParser(scrapy.Spider):
     def parse_car_details(self, response):
         model = response.meta['meta_model']
         car = response.css("div.car-list-item div.car-details-wrapper")
-        self.logger.info(response.url)
 
         loader = AutoUncleLoader(item=AutoUncleItem(), selector=car)
         loader.add_value('model', unicode(model))
